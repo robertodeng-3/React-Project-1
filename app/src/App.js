b5b3d26475dd1d366fc542c327d259a0f566f19e
robertodeng-3 <robertodeng3@gmail.com>
2021-08-17 21:12:52 +0100

    Add first reusable componant

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,16 @@
+import Modal from './componants/Modal';
+import Todo from './componants/Todo';
+import Backdrop from './componants/Backdrop';
 
 function App() {
-
-    return <div>Hello</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+          <Todo text='Learn React' />
+          <Modal />
+          <Backdrop />
+    </div>
+  );
 }
 
 export default App;
